44ecc10841c9a124cf4db4b2d5d31d960316c684
Erastus Ruiru <erastusruiru@Erastuss-MacBook-Pro.local>
2018-12-19 18:25:45 +0300

    ft-[finishes #162759357] user can create a todo item

--- todo/todoapp/views.py
+++ todo/todoapp/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import generics
+from .serializers import TodoListSerializer
+from .models import TodoList 
+
+
+class CreateView(generics.ListCreateAPIView):
+    """
+    This dictates how our todolist api will behave.
+
+    """
+    queryset = TodoList.objects.all()
+    serializer_class = TodoListSerializer
+
+    def perform_create(self, serializer):
+        """Save the post data when creating a new Todolist item."""
+        serializer.save()
