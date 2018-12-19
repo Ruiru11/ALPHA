f98307b49f052a1fd12abfdf18b0511187a731cd
erick wachira <wachira@ericks-MacBook-Pro.local>
2018-12-19 17:18:53 +0300

    [starts #162758846] Creatd test to fetch all todoitems and one specific

--- todo/todoapp/tests.py
+++ todo/todoapp/tests.py
@@ -1,3 +1,37 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase, APIRequestFactory
 
-# Create your tests here.
+from todoapp import views
+
+class TestTodos(APITestCase):
+    """
+        Tests all endpoints involving todos
+    """
+    def setUp(self):
+        """
+            This is the setup method
+        """
+        self.factory = APIRequestFactory()
+        self.view = views.TodoView.as_view()
+        self.uri = '/todos/'
+
+    def test_get_all_todos(self):
+        """
+            Test fetching all todo items
+        """
+        request = self.factory.get(self.uri)
+        response = self.view(request)
+        self.assertEqual(
+            response.status_code, 200
+        )
+
+    def test_get_one_specific(self):
+        """
+            Test fetching a specific todo item
+        """
+        request = self.factory.get(self.uri + '/1/')
+        response = self.view(
+            request
+        )
+        self.assertEqual(
+            response.status_code, 200
+        )
